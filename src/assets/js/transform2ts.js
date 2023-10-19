d8b1d35fe35784ecadc6ebcdd7b3d6b89089aa9c
harry <1751826104@qq.com>
2023-10-19 14:04:18 +0800

    feat: 调整工具算法

---
+++ src/assets/js/transform2ts.js
@@ -0,0 +1,84 @@
+const createTypeDefine = (jsonData) => {
+  const transTypeMap = {
+    string: "string",
+    integer: "number",
+    array: "[]",
+  };
+
+  const prefixIndent = (str, indent = 0, step = "  ") => {
+    return `${step.repeat(indent)}${str}`;
+  };
+  const addRow = (str, indent, step) => {
+    return `${prefixIndent(str, indent, step)}\n`;
+  };
+
+  const getDescription = (value) => {
+    return value.description ? `// ${value.description}` : "";
+  };
+
+  const transProperties = (value, options) => {
+    const { indent, prefix = "", suffix = "", append = addRow } = options;
+    const nextIndent = indent + 2;
+    let str = prefix;
+    let flag = false;
+    if (value.originalRef) {
+      str += `${value.originalRef}`;
+    } else {
+      switch (value.type) {
+        case "object":
+          str += `{`;
+          for (const key in value.properties) {
+            if (!flag) {
+              str += addRow("");
+            }
+            flag = true;
+            const desc = getDescription(value.properties[key]);
+            if (desc) {
+              str += addRow(desc, nextIndent);
+            }
+            str += addRow(
+              transProperties(value.properties[key], {
+                prefix: `${key}: `,
+                indent: nextIndent,
+                append: prefixIndent,
+                suffix: ";",
+              }),
+              nextIndent
+            );
+          }
+          str += prefixIndent("}", flag ? indent : 0);
+          break;
+        case "array":
+          str += `${transProperties(value.items, {
+            indent,
+            append: prefixIndent,
+          })}${transTypeMap[value.type]}`;
+          break;
+        default:
+          str += `${transTypeMap[value.type]}`;
+      }
+    }
+    str += suffix;
+    return str;
+  };
+  const transDefineToType = (jsonSchema, indent = 0) => {
+    let str = "";
+    str += addRow(`// ${jsonSchema.title}`, indent);
+    str += transProperties(jsonSchema, {
+      indent,
+      prefix: `export type ${jsonSchema.title} = `,
+      suffix: ";",
+    });
+    str += addRow("", indent);
+    return str;
+  };
+  const { definitions } = jsonData;
+  let result = "";
+  for (const key in definitions) {
+    const value = definitions[key];
+    result += addRow(`${transDefineToType(value)}`);
+  }
+  return result;
+};
+
+export { createTypeDefine };
